56bdfa939021378f8258a116a5a905f922914fc5
hanifrobbani <mhanifrobbani51@gmail.com>
2025-06-10 16:42:46 +0700

    fix: uncomment middleware & add example constant data

---
+++ src/constant/global.ts
@@ -0,0 +1,4 @@
+//contant data example
+export const SITE_NAME = "Contant data";
+export const SITE_DESCRIPTION = "Testing data constant";
+export const SITE_URL = "https://example-url.com";
--- src/middleware.ts
+++ src/middleware.ts
@@ -5,14 +5,14 @@ export function middleware(request: NextRequest) {
     const token = request.cookies.get("token")?.value;
     const currentUrl = request.nextUrl.pathname;
 
-    // if (token && currentUrl === "/login") { //url can be custom
-    //     return NextResponse.redirect(new URL("/", request.url));
-    // }
+    if (token && currentUrl === "/login") { //url can be custom
+        return NextResponse.redirect(new URL("/", request.url));
+    }
 
-    // if (!token && !currentUrl.startsWith("/login")) { //url can be custom
-    //     const loginUrl = new URL("/login", request.url);
-    //     return NextResponse.redirect(loginUrl);
-    // }
+    if (!token && !currentUrl.startsWith("/login")) { //url can be custom
+        const loginUrl = new URL("/login", request.url);
+        return NextResponse.redirect(loginUrl);
+    }
 
     return NextResponse.next();
 }
