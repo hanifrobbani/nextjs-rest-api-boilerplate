afb2a8fec88ead2782b8cbdd2e56b29f81933ac8
hanifrobbani <mhanifrobbani51@gmail.com>
2025-06-11 22:16:59 +0700

    feat: update axios bearer token to http only

--- src/lib/axiosInstance.tsx
+++ src/lib/axiosInstance.tsx
@@ -3,13 +3,10 @@ import axios from "axios";
 const axiosInstance = axios.create({
 	baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
 	timeout: 10000,
+	withCredentials: true,
 	headers: {
 		"Content-Type": "application/json",
 	},
 });
-
-export const setTokenAxiosInstance = (token: string) => {
-	axiosInstance.defaults.headers.common.Authorization = `Bearer ${token}`;
-};
 
 export default axiosInstance;
